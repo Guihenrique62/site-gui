88b18b27c4e80ad6033c11943f840618a3fdc0a7
Guilherme <guilhermeportosantos1@gmail.com>
2025-08-18 21:11:34 -0300

    Add Header Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Header } from "@/components/header";
+import { Button } from "@/components/ui/button";
+import { Geist, Geist_Mono } from "next/font/google"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  subsets: ["latin"]
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  subsets: ["latin"]
+})
 
 export default function Home() {
   return (
-    <div className="min-h-full min-w-full ">
-      <h2>Hello Word</h2>
+    <div className={
+      `${geistSans.variable} ${geistMono.variable}
+       grid grid-rows-[20px_1fr_20px]
+       items-center justify-items-center min-h-screen p-8
+       pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]
+      `
+    }>
+      <Header/>
+
+      <Button variant="secondary"></Button>
     </div>
   );
 }
