d3327746c11af24e5f9f03e7a47f683335727598
Guilherme <guilhermeportosantos1@gmail.com>
2025-08-18 21:14:28 -0300

    Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,9 +21,6 @@ export default function Home() {
        pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]
       `
     }>
-      <Header/>
-
-      <Button variant="secondary"></Button>
     </div>
   );
 }
