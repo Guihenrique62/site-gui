51cbb6b2b420ee58f97df471fd172ca0bf7467cc
Guilherme <guilhermeportosantos1@gmail.com>
2025-08-18 22:00:00 -0300

    Fix styles and Font - Header and Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -7,16 +7,16 @@ export const Footer = ( ) => {
 
 
     return (
-        <footer className="border-t border-white/10">
+        <footer className="bg-gray-500">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between md:flex-row  gap-8 py-8">
                     
                     <Logo />
                     
-                    <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
-                        <Link href={"/terms-of-use"} className="hover:text-primary">Terms of use</Link>
-                        <Link href={"/privacy-police"} className="hover:text-primary">Privacy police</Link>
-                        <Link href={"/feedback"} className="hover:text-primary">Feedback</Link>
+                    <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-blue-100">
+                        <Link href={"/terms-of-use"} className="hover:text-blue-200">Terms of use</Link>
+                        <Link href={"/privacy-police"} className="hover:text-blue-200">Privacy police</Link>
+                        <Link href={"/feedback"} className="hover:text-blue-200">Feedback</Link>
                     </nav>
                 </div>
             </div>
