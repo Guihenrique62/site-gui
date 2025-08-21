9d3af304fe973971694c39955b5b70e4b31283c1
Guilherme <guilhermeportosantos1@gmail.com>
2025-08-21 20:52:54 -0300

    add map of custumers object

--- src/components/customer-story-section/custumer-story-section.tsx
+++ src/components/customer-story-section/custumer-story-section.tsx
@@ -1,11 +1,12 @@
 import { PT_Sans_Caption } from "next/font/google"
+import Image from "next/image"
 
 const ptSansCaption = PT_Sans_Caption({
     subsets: ['latin'],
     weight: '700'
 })
 
-const custumerStory = [
+const custumerStories = [
     {
         content: 'Creating my store with site.set was the best decision for my business. The platform is super intuitive, and I was able to put my products up for sale in just a few minutes.',
         author: {
@@ -33,9 +34,28 @@ export const CustumerStorySection = () => {
                 <h2 className={`${ptSansCaption.className} text-heading-xl text-gray-100 `}>Those who use it, approve!</h2>
 
                 <div className="grid gap-8 md:grid-cols-2">
+                    {custumerStories.map((custumerStory) => (
+                        <div key={custumerStory.author.name} className="flex flex-col gap-6 rounded-lg bg-gray-500 p-6 md:p-12">
+                            <p className="text-gray-200 text-balance">
+                                {custumerStory.content}
+                            </p>
+                            <div className="flex items-center gap-3 ">
+                                <div className="relative h-10 w-10 overflow-hidden rounded-full">
+                                    <Image 
+                                        src={custumerStory.author.avatar}
+                                        alt={custumerStory.author.name}
+                                        fill
+                                        className="object-cover"
+                                    />
+                                </div>
 
-                    
-
+                                <div className="flex flex-col">
+                                    <strong className="text-gray-200 text-sm">{custumerStory.author.name}</strong>
+                                    <span className="text-xs text-gray-300">{custumerStory.author.role}</span>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
 
