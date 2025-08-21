7178a4aa24dc1ac2b2cfb058cd737369ae3ab223
Guilherme <guilhermeportosantos1@gmail.com>
2025-08-20 21:02:02 -0300

    FEAT Feature Section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 
+import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
     <article className="flex flex-col">
       <HeroSection /> 
+      <FeatureSection />
     </article>
     </>
   );
