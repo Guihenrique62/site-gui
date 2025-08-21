ed9a7d6494cd21b7aecb8cc86ea93890bd7ec944
Guilherme <guilhermeportosantos1@gmail.com>
2025-08-21 20:39:42 -0300

    initial html custumer-story-section

--- src/components/customer-story-section/custumer-story-section.tsx
+++ src/components/customer-story-section/custumer-story-section.tsx
@@ -1,8 +1,22 @@
+import { PT_Sans_Caption } from "next/font/google"
+
+const ptSansCaption = PT_Sans_Caption({
+    subsets: ['latin'],
+    weight: '700'
+})
 export const CustumerStorySection = () => {
 
     return (
-        <section>
-            
+        <section className="container py-8 md:py-10 ">
+            <div className="flex flex-col gap-12 items-center">
+
+                <h2 className={`${ptSansCaption.className} text-heading-xl text-gray-100 `}>Those who use it, approve!</h2>
+
+                <div className="grid gap-8 md:grid-cols-2">
+
+                </div>
+            </div>
+
         </section>
     )
 }
