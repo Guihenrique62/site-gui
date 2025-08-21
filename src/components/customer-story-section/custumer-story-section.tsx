a6468d429e0e3d758907ab2301be003da73961d0
Guilherme <guilhermeportosantos1@gmail.com>
2025-08-21 20:45:16 -0300

    Add object of custumers

--- src/components/customer-story-section/custumer-story-section.tsx
+++ src/components/customer-story-section/custumer-story-section.tsx
@@ -4,6 +4,26 @@ const ptSansCaption = PT_Sans_Caption({
     subsets: ['latin'],
     weight: '700'
 })
+
+const custumerStory = [
+    {
+        content: 'Creating my store with site.set was the best decision for my business. The platform is super intuitive, and I was able to put my products up for sale in just a few minutes.',
+        author: {
+            name: 'Annette Bones',
+            role: 'CEO Anne Corp',
+            avatar: '/avatar/custumer-01.png'
+        }
+    },
+    {
+        content: 'Turning my idea into an online store was quick and easy. I loved the customization options and the ease of order management. I can already see my products reaching more people!',
+        author: {
+            name: 'Jacob Jones',
+            role: 'CEO JJ Org',
+            avatar: '/avatar/custumer-02.png'
+        }
+    }
+]
+
 export const CustumerStorySection = () => {
 
     return (
@@ -13,6 +33,8 @@ export const CustumerStorySection = () => {
                 <h2 className={`${ptSansCaption.className} text-heading-xl text-gray-100 `}>Those who use it, approve!</h2>
 
                 <div className="grid gap-8 md:grid-cols-2">
+
+                    
 
                 </div>
             </div>
